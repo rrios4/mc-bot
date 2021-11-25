20cdf23b38bfad62750f13fe3358b1fc58a0c2fd
Rogelio Rios <rogeliorios348@gmail.com>
2021-11-25 15:35:09 -0600

    Scrapping DDR5 page data - Added ability to get data for product url, img, prices, titles, stock status, product availability, and product stock status

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,16 +1,60 @@
 const scraperObject = {
     url: 'https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966965+4294815592&myStore=false',
+    //url: 'https://www.microcenter.com/category/4294966995/all-processors',
     async scraper(browser){
         let page = await browser.newPage();
         console.log(`Navigating to  ${this.url}...`);
         await page.goto(this.url)
         //Wait for the DOM to be rendered
-        await page.waitForSelector('body');
+        await page.waitForSelector('#productGrid');
         //Get the link to all products
-        let urls = await page.$$eval('article ul > li', links => {
-            return links
+        let urls = await page.$$eval('article > ul > li', links => {
+            links = links.map(el => el.querySelector('h2 > a').href)
+            return links;
         }) 
         console.log(urls);
+
+        let prices = await page.$$eval('article > ul > li', prices => {
+            prices = prices.map(el => el.querySelector('span[itemprop=price]').textContent)
+            return prices;
+        }) 
+        console.log(prices);
+
+        let imgURLS = await page.$$eval('article > ul > li', urls => {
+            urls = urls.map(el => el.querySelector('img').src)
+            return urls
+        })
+        console.log(imgURLS);
+
+        let productTitles = await page.$$eval('article > ul > li', titles => {
+            titles = titles.map(el => el.querySelector('h2 > a').textContent)
+            return titles
+        })
+        console.log(productTitles)
+
+        let productSKU = await page.$$eval('article > ul > li', stockStatus => {
+            stockStatus = stockStatus.map(el => el.querySelector('p').textContent)
+            return stockStatus
+        })
+        console.log(productSKU)
+
+        let productAvailability = await page.$$eval('article > ul > li', stockStatus => {
+            stockStatus = stockStatus.map(el => el.querySelector('.instore.buyingrestriction').textContent)
+            return stockStatus
+        })
+        console.log(productAvailability)
+
+        let productStockStatus = await page.$$eval('article > ul > li', stockStatus => {
+            stockStatus = stockStatus.map(el => el.querySelector('.stock').textContent);
+            return stockStatus
+        })
+        //console.log(productStockStatus[0].replace('\n',''))
+
+        // let buttonInfo = await page.$$eval('article > ul > li', productButtonInfo => {
+        //     productButtonInfo = productButtonInfo.map(el => el.querySelector('input[aria-label="Add SKU#301259 to cart"]'))
+        //     return productButtonInfo
+        // })
+        // console.log(buttonInfo);
     }
 }
 
